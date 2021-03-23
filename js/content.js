3255022c504cd05e69bd6b6e2259212fb0d4ee27
febriasahrina <febriasahrina@gmail.com>
2021-03-23 13:57:26 +0700

    upload all files

---
+++ js/content.js
@@ -0,0 +1,216 @@
+//header 
+document.getElementById("header").innerHTML="<div class='navi'>"+
+												"<div class='icon' onclick='openNav()'><a href='#'>&#9776;</a></div>"+
+												"<div class='image'><a href='#home' id='home1'>&nbsp</a></div>"+
+												"<div class='tombol'><a href='#home' id='home'>Home</a></div>"+
+												"<div class='tombol dropdown_parent'><a href='#Product' id='product'>Product</a>"+
+													"<div class='dropdown'>"+
+														"<a href='#Boots' id='Boots'><div class='dropdown_child'>Sepatu Boots</div></a>"+
+														"<a href='#Holiday' id='Holiday'><div class='dropdown_child'>Sepatu Holiday</div></a>"+
+														"<a href='#kantor' id='kantor'><div class='dropdown_child'>Sepatu kantor</div></a>"+
+														"<a href='#Signore' id='Signore'><div class='dropdown_child'>Signore Venture</div></a>"+
+														"<a href='#Sneakers1' id='Sneakers1'><div class='dropdown_child'>Sneakers</div></a>"+
+													"</div>"+
+												"</div>"+
+												"<div class='tombol'><a href='#video' id='video'>Video</a></div>"+
+												"<div class='tombol''><a href='#guestbook' id='guestbook'>Guestbook</a></div>"+
+												"<div class='tombol'><a href='#About' id='about'>About</a></div>"+
+												"<div class='tombol'><a href='#contact' id='contact'>Contact</a></div>"+
+												"<div class='search1'><form id='cariform' class='search'><input id='caritext' type='search' name='search' placeholder='Search..'></form></div>"+
+												"<div class='login'><a href='#login' id='login'><span class='tooltiptext'>login</span></a></div>"+
+											"</div>"+
+											"<div class='menu_responsive' id='responsive'>"+
+												"<a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>&times;</a>"+
+												"<div><a href='#' class='parent-kategori'> Product</a>"+
+													"<div class='categori' style='display: none;'>"+
+														"<a href='#Boots' id='Boots1'>Sepatu Boots</a>"+
+														"<a href='#Holiday' id='Holiday1'>Sepatu Holiday</a>"+
+														"<a href='#kantor' id='kantor1'>Sepatu kantor</a>"+
+														"<a href='#Signore' id='Signore1'>Signore Venture</a>"+
+														"<a href='#Sneakers1' id='Sneakers2'>Sneakers</a>"+
+													"</div>"+
+												"</div>"+
+												"<a href='#video' id='video1'>Video</a>"+
+												"<a href='#guestbook' id='guestbook1'>Guestbook</a>"+
+												"<a href='#About' id='about1'>About</a>"+
+												"<a href='#contact' id='contact1'>Contact</a>"+
+											"</div>";
+
+//footer
+document.getElementById("footer").innerHTML="<div class='container_a'"+
+													"<div class='payment'>"+
+														"<img class='paymentimage' src='' id='payment'"+
+													"</div>"+
+											"<div";
+document.getElementById("footer1").innerHTML="<div class='footer_padding'>"+
+												"<div class='container1'>"+
+													"<div class='row'>"+
+														"<ul class='raijin'>"+
+															"<li>"+
+																"<span class='sosmed_line'>"+
+																"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+																"</span>"+
+															"</li>"+
+															"<li>"+
+																"<a href='https://id-id.facebook.com/BrodoIndonesia/'><img class='footer_sosmed' src='#facebook' id='facebook'></a>"+
+															"</li>"+
+															"<li>"+
+																"<a href=''><img class='footer_sosmed' src='' id='instagram'></a>"+
+															"</li>"+
+															"<li>"+
+																"<a href=''><img class='footer_sosmed' src='' id='twitter'></a>"+
+															"</li>"+
+															"<li>"+
+																"<a href=''><img class='footer_sosmed' src='' id='google'></a>"+
+															"</li>"+
+															"<li>"+
+																"<a href=''><img class='footer_sosmed' src='' id='WA'></a>"+
+															"</li>"+
+															"<li>"+
+																"<a href=''><img class='footer_sosmed' src='' id='youtube'></a>"+
+															"</li>"+
+															"<li>"+
+																"<span class='sosmed_line'>"+
+																"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+																"</span>"+
+															"</li>"+
+														"</ul>"+
+													"</div>"+
+												"</div"+
+											 "</div>";
+document.getElementById("footer2").innerHTML="<div class='wide_container_home'>"+
+												"<div class='container2'>"+
+													"<div style='margin:auto; padding-left: 20px;'>"+
+														"<div class='footwear'>"+
+															"<section class='span5 block'>"+
+																"<h4 class='title_block'>"+
+																	"BRODO FOOTWEAR"+
+																"</h4>"+
+																"<ul class='list_footer'>"+
+																	"<li>"+
+																		"<a href='' id='About'>TENTANG KAMI</a>"+
+																	"</li>"+
+																	"<li>"+
+																		"<a href='#test'>cara pemesanan</a>"+
+																	"</li>"+
+																	"<li>"+
+																		"<a href='#test'>Toko reseller masbro</a>"+
+																	"</li>"+
+																	"<li>"+
+																		"<a href='#test' id='hubungi_kami'>hubungi kami</a>"+
+																	"</li>"+
+																	"<li>"+
+																		"<a href='#test'>konfirmasi pembayaran</a>"+
+																	"</li>"+
+																"</ul>"+
+															"</section>"+
+														"</div>"+
+														"<div class='footwear'>"+
+															"<section class='span5 block'>"+
+																"<h4 class='title_block'>LEGAL</h4>"+
+																"<ul class='list_footer list_footer1'>"+
+																	"<li>"+
+																		"<a href='' id='syarat_dan_ketentuan'>syarat dan kondisi penggunaan</a>"+
+																	"</li>"+
+																	"<li>"+
+																		"<a href='#test'>kebijakan privasi</a>"+
+																	"</li>"+
+																"</ul>"+
+																
+																"<ul class='list_footer list_footer1'>"+
+																	"<li>"+
+																		"<h4 style='margin:3px 0px;' class='title_block'>Lainnya</h4>"+
+																	"</li>"+
+																	"<li>"+
+																		"<a href='#test' id='halaman_video'>halaman video</a>"+
+																	"</li>"+
+																	"<li>"+
+																		"<a href='#test' id='sitemap'>site map</a>"+
+																	"</li>"+
+																"</ul>"+
+															"</section>"+
+														"</div>"+
+														"<div class='foot_contact'>"+
+															"<section class='span3 block'>"+
+																"<h4 class='title_block'>"+
+																	"hubungi kami"+
+																"</h4>"+
+																"<ul class='list_footer list_footer2'>"+
+																	"<li>"+
+																		"Layanan pelanggan kami beroperasi setiap hari awal pekan mulai pukul 08.00 hingga 22.00 & akhir pekan mulai pukul 10.00 hingga 20.00<br>"+
+																		"Layanan Pelanggan: 08118545555<br>"+
+																		"Email: INFO@BRO.DO"+
+																	"</li>"+
+																"</ul>"+
+															"</section>"+
+														"</div>"+
+													"</div>"+
+												"</div>"+
+											 "</div>";
+
+function setupcari(lokasi) {//lokasi untuk menentukan letak folder utama dari file yang di akses
+
+	$("#home").attr("href",lokasi+"Home.html");
+	$("#home1").attr("href",lokasi+"Home.html");
+	$("#sitemap").attr("href",lokasi+"site_map.html");
+	$("#About").attr("href",lokasi+"About.html");
+	$("#syarat_dan_ketentuan").attr("href",lokasi+"syarat_dan_ketentuan.html");
+	$("#product").attr("href",lokasi+"Website/Web_Semua_Produk/semua_produk.html");
+	$("#Boots").attr("href",lokasi+"Website/Web_Product/SepatuBoots.html");
+	$("#Holiday").attr("href",lokasi+"Website/Web_Product/SepatuHoliday.html");
+	$("#kantor").attr("href",lokasi+"Website/Web_Product/SepatuKantor.html");
+	$("#Signore").attr("href",lokasi+"Website/Web_Product/SignoreVenture.html");
+	$("#Sneakers1").attr("href",lokasi+"Website/Web_Product/Sneakers.html");
+	$("#Boots1").attr("href",lokasi+"Website/Web_Product/SepatuBoots.html");
+	$("#Holiday1").attr("href",lokasi+"Website/Web_Product/SepatuHoliday.html");
+	$("#kantor1").attr("href",lokasi+"Website/Web_Product/SepatuKantor.html");
+	$("#Signore1").attr("href",lokasi+"Website/Web_Product/SignoreVenture.html");
+	$("#Sneakers2").attr("href",lokasi+"Website/Web_Product/Sneakers.html");
+	$("#guestbook").attr("href", lokasi+"Website/Guestbook/Guestbook.html")	
+	$("#video").attr("href", lokasi+"Website/Web_video/Halaman_video.html")
+	$("#about").attr("href",lokasi+"About.html");
+	$("#contact").attr("href",lokasi+"Contact.html");
+	$("#hubungi_kami").attr("href",lokasi+"Contact.html");
+	$("#halaman_video").attr("href", lokasi+"Website/Web_video/Halaman_video.html")
+	$("#video1").attr("href", lokasi+"Website/Web_video/Halaman_video.html")
+	$("#about1").attr("href",lokasi+"About.html");
+	$("#contact1").attr("href",lokasi+"Contact.html");
+	$("#guestbook1").attr("href", lokasi+"Website/Guestbook/Guestbook.html")	
+	$("#login").attr("href",lokasi+"Website/Form_daftar/login.html");
+	$("#masbro").attr("src",lokasi+"Image/home_image/janji/logo_putih.png");
+	$("#facebook").attr("src",lokasi+"Image/footer_image/sosmed/facebook.png");
+	$("#instagram").attr("src",lokasi+"Image/footer_image/sosmed/instagram.png");
+	$("#twitter").attr("src",lokasi+"Image/footer_image/sosmed/twitter.png");
+	$("#google").attr("src",lokasi+"Image/footer_image/sosmed/google+.png");
+	$("#WA").attr("src",lokasi+"Image/footer_image/sosmed/WA.png");
+	$("#youtube").attr("src",lokasi+"Image/footer_image/sosmed/youtube.png");
+	$("#payment").attr("src",lokasi+"Image/footer_image/footer_feb_2015.png");
+
+	var textcari= location.hash;
+	textcari = textcari.replace(/#/g, '');
+	textcari = decodeURIComponent(textcari);//mengembalikan karakter yg telah di encode di URL, contohnya (space) di encode menjadi %20, ketika didecodemenjadi (space) kembali;
+
+	$("#caritext").val(textcari);
+
+	$("#cariform").submit(function(){
+
+		document.location=lokasi+"/Website/Web_Semua_Produk/semua_produk.html#"+$("#caritext").val();
+		
+		return false;
+	});
+}
+
+function openNav() {
+    document.getElementById("responsive").style.width = "80%";
+}
+function closeNav() {
+    document.getElementById("responsive").style.width = "0";
+}
+
+$('.parent-kategori').click(function(){
+	var parent=$(this).parent();
+
+	$('.categori',parent).toggle();
+	return false;
+
+});
